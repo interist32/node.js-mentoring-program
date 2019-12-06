31141585be630e081206acbdf006a0ecdc26dca0
interist32 <interist32@gmail.com>
2019-12-06 15:03:53 -0800

    Task 2.

--- hw-2/models/user.ts
+++ hw-2/models/user.ts
@@ -1,3 +1,5 @@
+import * as Joi from '@hapi/joi';
+
 /** User model. */
 export interface User {
   id: string;
@@ -6,3 +8,12 @@ export interface User {
   age: number;
   isDeleted: boolean;
 }
+
+/** User validation schema. */
+export const userSchema = Joi.object<User>({
+  id: Joi.string(),
+  login: Joi.string().required(),
+  password: Joi.string().regex(/^(?=.*[a-zA-Z])(?=.*[0-9])/).required(),
+  age: Joi.number().min(4).max(130).required(),
+  isDeleted: Joi.boolean(),
+});
--- hw-2/server.ts
+++ hw-2/server.ts
@@ -1,10 +1,25 @@
 import express = require('express');
 import {UsersStorage} from './storage/users';
 import bodyParser = require('body-parser');
+import {ContainerTypes,
+        // Use this as a replacement for express.Request
+        ValidatedRequest,
+        // Extend from this to define a valid schema type/interface
+        ValidatedRequestSchema,
+        // Creates a validator that generates middlewares
+        createValidator} from 'express-joi-validation';
+import {User, userSchema} from './models/user';
+
+interface UserRequestSchema extends ValidatedRequestSchema {
+  [ContainerTypes.Body]: User;
+}
+
+const PORT = 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
+const validator = createValidator();
 const usersStorage = new UsersStorage();
 
 app.get('/users', (req, res) => {
@@ -15,15 +30,23 @@ app.get('/users/:id', (req, res) => {
   res.json(usersStorage.getUserById(req.params.id));
 });
 
-app.post('/users', (req, res) => {
-  usersStorage.add(req.body.user);
-  res.json('Created');
-});
+app.post(
+    '/users', validator.body(userSchema),
+    (req: ValidatedRequest<UserRequestSchema>, res) => {
+      usersStorage.add(req.body);
+      res.json('Created');
+    });
 
-app.put('/users/:id', (req, res) => {
-  usersStorage.update(req.body.user);
-  res.json('Updated');
-});
+app.put(
+    '/users/:id', validator.body(userSchema),
+    (req: ValidatedRequest<UserRequestSchema>, res) => {
+      const user: User = {
+        ...req.body,
+        id: req.params.id,
+      };
+      usersStorage.update(user);
+      res.json('Updated');
+    });
 
 app.delete('/users/:id', (req, res) => {
   usersStorage.remove(req.params['id']);
@@ -35,6 +58,7 @@ app.get('/getAutoSuggestUsers', (req, res) => {
       req.query.login, Number(req.query.limit) || undefined));
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening');
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
