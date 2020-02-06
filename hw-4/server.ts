364fc13a450ba7efac916b41f6c87ed062f854c1
interist32 <interist32@gmail.com>
2020-02-06 12:38:04 -0800

    Copy hw-2 to hw-4.

---
+++ hw-4/server.ts
@@ -0,0 +1,17 @@
+require('module-alias/register');
+
+// eslint-disable-next-line import/first
+import usersRouter from './routers/user.router';
+
+import express = require('express');
+import bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
