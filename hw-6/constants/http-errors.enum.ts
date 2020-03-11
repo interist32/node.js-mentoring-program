a7f17c8d9ce2c53e20d6a721aece69d135b39f0b
interist32-mi\interist32 <interist32@gmail.com>
2020-03-11 10:12:24 -0700

    More fixes from review.

--- hw-6/constants/http-errors.enum.ts
+++ hw-6/constants/http-errors.enum.ts
@@ -7,3 +7,4 @@ export enum HTTP_ERROR {
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 export const JWT_EXPIRATION_TIME_MS = process.env.JWT_EXPIRATION_TIME_MS || 10;
+export const JWT_HTTP_HEADER_KEY = 'x-access-token';
--- hw-6/middlewares/check-auth-token.ts
+++ hw-6/middlewares/check-auth-token.ts
@@ -1,25 +1,27 @@
-import { NextFunction, Request, Response } from 'express';
+import {NextFunction, Request, Response} from 'express';
 import * as jwt from 'jsonwebtoken';
-import { HTTP_ERROR, JWT_SECRET } from '../constants/http-errors.enum';
 
-export const checkAuthToken = (req: Request, res: Response, next: NextFunction): void => {
-  const token = req.headers['x-access-token'] as string | undefined;
+import {HTTP_ERROR, JWT_HTTP_HEADER_KEY, JWT_SECRET} from '../constants/http-errors.enum';
 
-  if (token) {
-    jwt.verify(token, JWT_SECRET, (err) => {
-      if (err) {
-        res.status(HTTP_ERROR.UNAUTHORIZED).json({
-          success: false,
-          message: 'Failed to authenticate token.',
+export const checkAuthToken =
+    (req: Request, res: Response, next: NextFunction): void => {
+      const token = req.headers[JWT_HTTP_HEADER_KEY] as string;
+
+      if (token) {
+        jwt.verify(token, JWT_SECRET, (err) => {
+          if (err) {
+            res.status(HTTP_ERROR.UNAUTHORIZED).json({
+              success: false,
+              message: 'Failed to authenticate token.',
+            });
+          } else {
+            next();
+          }
         });
       } else {
-        next();
+        res.status(HTTP_ERROR.UNAUTHORIZED)
+            .send({success: false, message: 'No token provided.'});
       }
-    });
-  } else {
-    res.status(HTTP_ERROR.UNAUTHORIZED)
-      .send({ success: false, message: 'No token provided.' });
-  }
-};
+    };
 
 export default checkAuthToken;
